1c6b9eb4d62b4bda2acf7ae0f664e5041447dab2
wholesomewilson <wilsonwan88@gmail.com>
2019-08-13 14:24:22 +0800

    init

--- app/javascript/components/DateTimePicker.js
+++ app/javascript/components/DateTimePicker.js
@@ -1,14 +1,14 @@
-import React, { Fragment, useState } from "react";
-import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
-import MomentUtils from '@date-io/moment';
-import moment from 'moment';
+//import React, { Fragment, useState } from "react";
+//import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+//import MomentUtils from '@date-io/moment';
+//import moment from 'moment';
 
 function BasicDateTimePicker({ handleDate, existingDate, pickerlabel }) {
-  let newDate = new Date();
-  const [selectedDate, handleDateChange] = useState(newDate);
+  //let newDate = new Date();
+  //const [selectedDate, handleDateChange] = useState(newDate);
   return (
     <div>
-      Time Picker
+      Hello!
     </div>
   );
 }
