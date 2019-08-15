71b2cb7f9d388ceebce81d070de8480182b39d9a
wholesomewilson <wilsonwan88@gmail.com>
2019-08-15 13:08:13 +0800

    init

--- app/javascript/components/AlarmInfo.js
+++ app/javascript/components/AlarmInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import AlarmIcon from '@material-ui/icons/Alarm':
+import AlarmIcon from '@material-ui/icons/Alarm';
 
 const AlarmInfo = ({ alarmmomenttime, alarmmomentdate }) => {
 
