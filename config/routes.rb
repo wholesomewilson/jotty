a372eeb86bb0b0aa9939bf8e30a69c47d1145658
wholesomewilson <wilsonwan88@gmail.com>
2019-08-15 11:27:13 +0800

    init

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'posts/:id', to: 'site#index'
   get 'posts/:id/edit', to: 'site#index'
   get 'setup', to: 'setup#index'
-  get 'teleupdates', 'setup#teleupdates'
+  get 'teleupdates', to: 'setup#teleupdates'
 
 
 
