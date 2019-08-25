7a4fe735cf7285b4814dbdb0647a636f761869c2
wholesomewilson <wilsonwan88@gmail.com>
2019-08-25 20:42:13 +0800

    init

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -22,15 +22,10 @@ class Api::PostsController < ApplicationController
     @recipient = User.find(params[:post][:recipient_id])
     if @recipient == current_user
       @post = current_user.own_posts.create(post_params.merge(poster: current_user))
-      respond_with(:api, @post, :include => add_attr_post, :except => remove_attr_post)
     else
-      if @recipient.friends.include? current_user
-        @post = @recipient.own_posts.create(post_params.merge(poster: current_user))
-        respond_with(:api, @post, :include => add_attr_post, :except => remove_attr_post)
-      else
-        
-      end
+      @post = @recipient.own_posts.create(post_params.merge(poster: current_user))
     end
+    respond_with(:api, @post, :include => add_attr_post, :except => remove_attr_post)
   end
 
   def destroy
